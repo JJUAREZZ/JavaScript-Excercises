c696138cb3e0340da77d5464c99bd68f334b4dae
Joaco Juarez <jojuarez@frba.utn.edu.ar>
2025-03-25 23:42:29 -0300

    dia 3

---
+++ HackerRank/1 Week Preparation/Day 3/Caesar Cipher.js
@@ -0,0 +1,97 @@
+/*Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Example
+
+
+The alphabet is rotated by , matching the mapping above. The encrypted string is .
+
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+
+Function Description
+
+Complete the caesarCipher function in the editor below.
+
+caesarCipher has the following parameter(s):
+
+string s: cleartext
+int k: the alphabet rotation factor
+Returns
+
+string: the encrypted string
+Input Format
+
+The first line contains the integer, , the length of the unencrypted string.
+The second line contains the unencrypted string, .
+The third line contains , the number of letters to rotate the alphabet by.
+
+Constraints
+
+
+
+ is a valid ASCII string without any spaces.
+
+Sample Input
+
+11
+middle-Outz
+2
+Sample Output
+
+okffng-Qwvb
+Explanation
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b*/
+
+function caesarCipher(s, k) {
+    // Write your code here
+    let cipherArray = Array.from(s)
+    let alphabetLowerCase = Array.from("abcdefghijklmnopqrstuvwxyz")
+    let alphabetUperCase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    
+    let regexLetters = /[a-zA-Z]/;
+    for(let i = 0; i < cipherArray.length; i++){
+        let letterIndex = 0;
+        if(/^[a-zA-Z]/.test(String(cipherArray[i])) === true){
+            for(let j = 0; j <= alphabetLowerCase.length; j++){
+                if(cipherArray[i] === alphabetLowerCase[j]){
+                    if(j + k > alphabetLowerCase.length){
+                        letterIndex = k - (alphabetLowerCase.length - j)
+                        cipherArray[i] = alphabetLowerCase[letterIndex]
+                        break;
+                    }else {
+                        letterIndex = j;
+                        cipherArray[i] = alphabetLowerCase[letterIndex + k]
+                        break;
+                    }
+                } else if(cipherArray[i] === alphabetUperCase[j]) {
+                    if(j + k > alphabetUperCase.length){
+                        letterIndex = k - (alphabetUperCase.length - j)
+                        cipherArray[i] = alphabetUperCase[letterIndex]
+                        break;
+                    } else {
+                        letterIndex = j;
+                        cipherArray[i] = alphabetUperCase[letterIndex + k]
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+    return cipherArray.join("");
+}
